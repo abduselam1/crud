2c18cfcfcaf1ca8b10ac1011ffbcfc2840ef2806
abduselam1 <abduselamhafiz@gmail.com>
2022-11-24 11:45:42 +0300

    sql file updated

---
+++ check.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = [1,2,3,4,5];
+
+// echo "1" + 3;
+// echo $a[count($a)-1];
+$a = "abduselam";
+
+
+foreach($b as $a){
+    echo $a;
+}
+// print_r(str_split($a));
+// echo $a*2;
--- sql/crud.sql
+++ sql/crud.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 15, 2022 at 12:16 PM
+-- Generation Time: Nov 24, 2022 at 09:44 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.9
 
@@ -20,6 +20,52 @@ SET time_zone = "+00:00";
 --
 -- Database: `crud`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `name`) VALUES
+(1, 'Urgent'),
+(2, 'Medium');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `todos`
+--
+
+CREATE TABLE `todos` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `body` longtext NOT NULL,
+  `is_pinned` tinyint(1) NOT NULL DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `category_id` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `todos`
+--
+
+INSERT INTO `todos` (`id`, `title`, `body`, `is_pinned`, `created_at`, `updated_at`, `user_id`, `category_id`) VALUES
+(2, 'title2 ', 'Working on my project', 1, '2022-11-23 05:14:06', '2022-11-23 05:14:06', 1, 2),
+(4, 'title3 ', 'Todo todo my todo project', 1, '2022-11-23 05:15:47', '2022-11-23 05:15:47', 1, 1),
+(5, 'Todo 4 ed', 'Todo for my todo project', 0, '2022-11-23 05:16:07', '2022-11-23 05:16:07', 1, 1),
+(6, 'Todo lists', 'Hello e', 0, '2022-11-23 05:16:41', '2022-11-23 05:16:41', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -48,6 +94,19 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`) VALUES
 --
 
 --
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `todos`
+--
+ALTER TABLE `todos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -57,6 +116,18 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `todos`
+--
+ALTER TABLE `todos`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
