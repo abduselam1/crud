c6ae273a02e6ae9084ef53a9160c4c0ede1db3c7
abduselam1 <abduselamhafiz@gmail.com>
2022-11-24 10:43:34 +0300

    fix

--- check.php
+++
@@ -1,8 +0,0 @@
-<?php
-// $geeks= "alert(\"Hello\")";
-// echo htmlspecialchars($geeks);
-// $newgeeks = filter_var($geeks, FILTER_SANITIZE_STRING);
-// echo $newgeeks;
-
-$title = filter_var("alert(\"hello\")",FILTER_UNSAFE_RAW);
-echo $title;
